0cfd1eb58db95957dace01f3dddd7a59f4a7bad7
gitCauchy <mailcauchy@gmail.com>
2019-06-20 16:08:56 +0800

    finish the Stack

--- c/SqStack.c
+++ c/SqStack.c
@@ -25,4 +25,50 @@ Status GetTop(SqStack *S,ElemType *e);
 Status Push(SqStack *S,ElemType *e);
 Status Pop(SqStack *S,ElemType *e);
 Status StackEmpty(SqStack *S);
+int main( int argc ,char *argv[]){
+
+}
+Status InitStack(SqStack *S){
+	S -> base = (ElemType *)malloc(STACK_INIT_SIZE * sizeof(ElemType));
+	if(!S -> base){
+		return OVERFLOW;
+	}
+	S -> top = S -> base;
+	S -> stackSize = STACK_INIT_SIZE;
+	return OK;
+}
+Status GetTop(SqStack *S ,ElemType *e){
+	if(S -> base = S -> top){// 空栈
+		return ERROR;
+	}
+	*e = *(S -> top - 1);
+	return OK;
+}
+Status Push(SqStack *S,ElemType *e){
+	if(S -> top - S -> base >= S -> stackSize){
+		S -> base = (ElemType *)realloc(S -> base,(STACK_INCREMENT + S -> stackSize)*sizeof(ElemType));
+		if(! S -> base){
+			return OVERFLOW;
+		}
+		S -> top = S -> base + S -> stackSize;
+		S -> stackSize += STACK_INCREMENT;
+	}
+	* (S -> top ++) = *e;
+	return OK;
+}
+Status Pop(SqStack *S,ElemType *e){
+	if(S -> top == S -> base){
+		return ERROR;
+	}
+	*e = *(S -> top -1);
+	S -> top --;
+	return OK;
+}
+Status StackEmpty(SqStack *S){
+	if(S -> top == S -> base){
+		return TRUE;
+	}else{
+		return FALSE;
+	}
+}
 
